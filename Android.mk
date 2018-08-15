431309ff1b1a52c2734506e61afe9206dcdb2a0d
darkinfusion <daddyfire366@gmail.com>
2018-08-15 13:40:27 -0700

    add

---
+++ Android.mk
@@ -0,0 +1,26 @@
+# Copyright © 2010, 2013
+#    Thorsten Glaser <t.glaser@tarent.de>
+# This file is provided under the same terms as mksh.
+
+LOCAL_PATH := $(call my-dir)
+
+# /system/etc/mkshrc
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := mkshrc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT)/etc
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+# /vendor/etc/mkshrc
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := mkshrc_vendor
+LOCAL_MODULE_STEM := mkshrc
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_ETC)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
